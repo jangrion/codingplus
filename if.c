716ae9f00df29b78ae12fd946e769a52b743ac84
aiadmin <codingplus_temp@zohomail.com>
2020-07-11 18:23:26 +0900

    if test

---
+++ if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int sel;
+	printf("123 중에서 골라");
+	scanf("%d",&sel);
+	printf("kill/n");
+	if (sel == 2)
+		printf("kill /n");
+	else if (sel == 3)
+		printf("kill /n");
+	else
+		printf("kill /n");
+	return 0;
+}
